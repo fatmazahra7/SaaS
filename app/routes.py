6d7cb0cd30fffbbf1e1f64239f748013cd7ccfc3
fatmazahra7 <fatimazh2244@gmail.com>
2025-12-30 13:47:33 +0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,3 @@ router = APIRouter()
 @router.get("/health")
 def healthcheck():
     return {"status": "healthy"}
-
-'''main.py = CEO
-
-routes.py = Department that handles endpoints
-
-We separated them to keep the system scalable and professional.
-'''
