d0a3236757234ec9bf35e327b8863bb6c665f279
fatmazahra7 <fatimazh2244@gmail.com>
2025-12-30 13:46:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI(
     title="SaaS Backend",
     description="Backend + AI Event SaaS Platform",
     version="1.0.0"
-)  #This matters in interviews!
+)
 
 app.include_router(router)
 
